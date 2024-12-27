e9571bc228b538fb7618942ce2fda7df49ec9c1c
zobirofkir <zobirofkir19@gmail.com>
2024-12-27 16:36:15 +0100

    add seo optimization

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="description" content="{{$description ?? 'أفضل العروض لبيع السيارات الجديدة والمستعملة.'}}">
+    <meta name="keywords" content="{{$keywords ?? 'بيع سيارات, سيارات مستعملة, سيارات جديدة, شراء سيارة, بيع سيارة, عروض سيارات'}}">
+    <meta name="author" content="{{config('app.name')}}">
+
+    <meta property="og:title" content="{{$title}}">
+    <meta property="og:description" content="{{$description ?? 'أفضل العروض لبيع السيارات الجديدة والمستعملة.'}}">
+    <meta property="og:image" content="{{$ogImage ?? 'default-car-image-url.jpg'}}">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:type" content="website">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="{{$title}}">
+    <meta name="twitter:description" content="{{$description ?? 'أفضل العروض لبيع السيارات الجديدة والمستعملة.'}}">
+    <meta name="twitter:image" content="{{$twitterImage ?? 'default-car-image-url.jpg'}}">
+
+    <link rel="icon" href="{{ asset('assets/images/logo.png') }}" type="image/png">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <title>{{$title}}</title>
