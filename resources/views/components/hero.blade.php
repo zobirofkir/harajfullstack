8d312e13cf456d19efb06eff28eac8d97832075f
zobirofkir <zobirofkir19@gmail.com>
2024-12-27 16:15:45 +0100

    updae hero image

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,4 +1,4 @@
-<div class="relative bg-cover bg-center h-[500px]" style="background-image: url('https://media.istockphoto.com/id/1386416562/fr/photo/c%C3%B4t%C3%A9-turquie.jpg?s=612x612&w=0&k=20&c=0--bmWHq106Z8bTCs1U56SN6mbmbrcC75PYy7lA0tyA=');">
+<div class="relative bg-cover bg-center h-[500px]" style="background-image: url('{{asset('assets/images/bm1.jpg')}}');">
     <div class="absolute inset-0 bg-black opacity-50"></div>
     <div class="container mx-auto py-24 px-4 text-center relative z-10">
         <h1 class="text-5xl font-extrabold text-white mb-4 uppercase tracking-wide">احصل على سيارتك المفضلة الآن</h1>
