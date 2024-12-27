c2fecc97349c336c81830d7d544f5348137090b8
zobirofkir <zobirofkir19@gmail.com>
2024-12-27 11:25:48 +0100

    create users group

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -20,6 +20,8 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'المستخدمون';
+
     protected static ?string $navigationLabel = 'المستخدمون';
 
     protected static ?string $pluralLabel = 'المستخدمون';
