84aa6af46041476df39b6f50380ef3bf17b7bad6
zobirofkir <zobirofkir19@gmail.com>
2024-12-30 07:56:31 +0100

    create register page

---
+++ app/Filament/Resources/RegisterResource/Pages/Auth/Register.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Resources\RegisterResource\Pages\Auth;
+
+use App\Filament\Resources\RegisterResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Component;
+
+use Filament\Pages\Auth\Register as BaseRegister;
+
+class Register extends BaseRegister
+{
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getNameFormComponent(),
+                        $this->getEmailFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getPasswordConfirmationFormComponent(),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
+    }
+
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use \App\Filament\Resources\RegisterResource\Pages\Auth\Register;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -29,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration(Register::class)
             ->colors([
                 'primary' => [
                     50 => '238, 242, 255',
