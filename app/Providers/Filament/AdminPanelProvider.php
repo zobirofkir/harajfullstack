2c2687545fd2a3c78932f2724bb6725783dca672
zobirofkir <zobirofkir19@gmail.com>
2024-12-30 08:53:45 +0100

    display auth user image

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -47,7 +47,7 @@ class AdminPanelProvider extends PanelProvider
                 ],
             ])
             ->brandLogo(function () {
-                return asset('assets/images/logo.png');
+                return asset('storage/' . (Auth::user() && Auth::user()->image ? Auth::user()->image : 'assets/images/logo.png'));
             })
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
