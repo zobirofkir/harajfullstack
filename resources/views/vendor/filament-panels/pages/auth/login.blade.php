efcdfa124f0dfef24454c7baeee43ed13d6dde6f
zobirofkir <zobirofkir19@gmail.com>
2025-01-18 15:28:09 +0100

    filament confi

---
+++ resources/views/vendor/filament-panels/pages/auth/login.blade.php
@@ -0,0 +1,30 @@
+<x-filament-panels::page.simple>
+    @if (filament()->hasRegistration())
+        <x-slot name="subheading">
+            {{ __('filament-panels::pages/auth/login.actions.register.before') }}
+
+            {{ $this->registerAction }}
+        </x-slot>
+    @endif
+
+    <div class="text-center mt-4">
+        <a href="{{url('/')}}" class="btn btn-link text-decoration-none text-muted">العودة إلى الصفحة الرئيسية</a>
+    </div>
+
+    <div class="text-center mt-4">
+        <a href="{{url('/admin/login')}}" class="btn btn-primary">تسجيل الدخول كمسؤول</a>
+    </div>
+
+    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_BEFORE, scopes: $this->getRenderHookScopes()) }}
+
+    <x-filament-panels::form id="form" wire:submit="authenticate" class="mt-4">
+        {{ $this->form }}
+
+        <x-filament-panels::form.actions
+            :actions="$this->getCachedFormActions()"
+            :full-width="$this->hasFullWidthFormActions()"
+        />
+    </x-filament-panels::form>
+
+    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_AFTER, scopes: $this->getRenderHookScopes()) }}
+</x-filament-panels::page.simple>
