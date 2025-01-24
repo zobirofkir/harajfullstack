91c3dc3242caaa6893533ab492bb5e9a980c55d1
zobirofkir <zobirofkir19@gmail.com>
2025-01-24 12:19:23 +0100

    add update usd to sarl

--- resources/views/emails/car_offer.blade.php
+++ resources/views/emails/car_offer.blade.php
@@ -8,6 +8,6 @@
     <h1>معلومات عرض السيارة</h1>
     <p><strong>عنوان السيارة:</strong> {{ $car->title }}</p>
     <p><strong>وصف السيارة:</strong> {{ $car->info }}</p>
-    <p><strong>سعر العرض القابل للتفاوض:</strong> ${{ $offer->negotiable_offer_price }}</p>
+    <p><strong>سعر العرض القابل للتفاوض:</strong>ريال {{ $offer->negotiable_offer_price }}</p>
 </body>
 </html>
