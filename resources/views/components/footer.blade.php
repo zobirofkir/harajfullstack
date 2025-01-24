3ed9d95819fe96346f1be4a9b5e624370c0bffe9
zobirofkir <zobirofkir19@gmail.com>
2025-01-24 22:11:24 +0100

    reset

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,25 +1,25 @@
-<footer class="bg-gray-100 text-orange-700 py-6 shadow-inner">
+<footer class="bg-gray-200 text-gray-700 py-6 shadow-inner">
     <div class="container mx-auto text-center">
         <p class="text-sm font-medium">
             &copy; <span>{{ date('Y') }}</span> دينالي. جميع الحقوق محفوظة.
         </p>
         <div class="flex justify-center gap-6 mt-4">
             <!-- Social Media Links -->
-            <a href="https://tiktok.com" class="text-orange-600 hover:text-orange-800">
+            <a href="https://tiktok.com" class="text-gray-600 hover:text-gray-800">
                 <i class="fa-brands fa-tiktok"></i>
             </a>
-            <a href="https://twitter.com" class="text-orange-600 hover:text-orange-800">
+            <a href="https://twitter.com" class="text-gray-600 hover:text-gray-800">
                 <i class="fa-brands fa-twitter-square"></i>
             </a>
-            <a href="https://instagram.com" class="text-orange-600 hover:text-orange-800">
+            <a href="https://instagram.com" class="text-gray-600 hover:text-gray-800">
                 <i class="fa-brands fa-instagram-square"></i>
             </a>
         </div>
         <div class="flex md:flex-row flex-col justify-center gap-6 mt-4">
-            <a href="{{url('/privacy')}}" class="text-orange-600 hover:text-orange-800 whitespace-nowrap">اتفاقية الاستخدام</a>
-            <a href="{{ url('/contacts') }}" class="text-orange-600 hover:text-orange-800 whitespace-nowrap">اتصل بنا</a>
+            <a href="{{url('/privacy')}}" class="text-gray-600 hover:text-gray-800 whitespace-nowrap">اتفاقية الاستخدام</a>
+            <a href="{{ url('/contacts') }}" class="text-gray-600 hover:text-gray-800 whitespace-nowrap">اتصل بنا</a>
             @if (!Auth::check())
-                <a href="{{ url('/login') }}" class="text-orange-600 hover:text-orange-800 whitespace-nowrap">تسجيل الدخول</a>
+                <a href="{{ url('/login') }}" class="text-gray-600 hover:text-gray-800 whitespace-nowrap">تسجيل الدخول</a>
             @else
 
             @endif
