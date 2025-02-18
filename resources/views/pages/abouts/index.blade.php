e47e093a128e0bd9b2d5579f0375b615ee360828
zobirofkir <zobirofkir19@gmail.com>
2025-02-18 16:06:59 +0100

    create mail

--- resources/views/pages/abouts/index.blade.php
+++ resources/views/pages/abouts/index.blade.php
@@ -4,14 +4,14 @@
             <div class="bg-white rounded-lg shadow-xl overflow-hidden p-8 md:p-12">
                 <!-- Text Section -->
                 <div class="max-w-2xl mx-auto text-center">
-                    <h1 class="text-4xl font-bold text-gray-800 mb-6">دينالي للسيارات</h1>
-                    <p class="text-gray-600 text-lg leading-relaxed mb-6">
+                    <h1 class="text-4xl font-bold text-blue-950 mb-6">دينالي للسيارات</h1>
+                    <p class="text-blue-950 text-lg leading-relaxed mb-6">
                         في دينالي نؤمن بأن بيع وشراء السيارات يجب أن يكون تجربة سلسة وشفافة لهذا السبب أنشأنا منصة تجمع بين البائع والمشتري بكل وضوح بعيدًا عن التعقيدات والمفاجآت غير المتوقعة.
                     </p>
-                    <p class="text-gray-600 text-lg leading-relaxed mb-6">
+                    <p class="text-blue-950 text-lg leading-relaxed mb-6">
                         دورنا لا يقتصر على توفير مساحة للإعلانات بل نكون الوسيط الموثوق بين الطرفين لضمان تجربة بيع وشراء آمنة عادلة وخالية من المتاعب من خلال معلومات دقيقة وأسعار شفافة ودعم مستمر نساعدك في اتخاذ القرار الصحيح بثقة وراحة.
                     </p>
-                    <p class="text-gray-600 text-lg leading-relaxed">
+                    <p class="text-blue-950 text-lg leading-relaxed">
                         مع دينالي الثقة أولاً والتجربة الأفضل دائمًا.
                     </p>
                 </div>
