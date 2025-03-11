a7dc88cb2b65215f2c37584f17e6946599d92738
zobirofkir <zobirofkir19@gmail.com>
2025-03-11 14:51:33 +0000

    fix error

--- database/migrations/2024_03_22_add_cover_photo_to_users.php
+++ database/migrations/2024_03_22_add_cover_photo_to_users.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('cover_photo')->nullable()->after('image');
+            $table->string('cover_photo')->nullable()->after('name');
         });
     }
 
